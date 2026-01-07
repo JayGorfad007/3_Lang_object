39c7b89d7bc5a5994f9c2327d51f4c4cbe9519fe
js-code <jaygorfad5977@gmail.com>
2026-01-07 18:29:35 +0530

    Multi pachs in word valid scanf

--- C_Lang/String_Ra/S4.c
+++ C_Lang/String_Ra/S4.c
@@ -1,5 +1,26 @@
+/*         L-1    29-12-25 Third_Lacture                           */ 
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    char 
+    char name[10];
+     
+
+    printf("\nEnter Name : ");
+    scanf("%[^\n]s",&name);  // scanf multipal word in pachs valid in "%[^\n]s",
+
+
+
+
+   // gets(name);  // Get String
+   // scanf("%s", name);          //scanf is a do not multipal scaning Pache
+
+    printf("\nName : %s",name);
+
+   // puts(name);
+
+    
+    return 0;
+
 }
+// scnaf is not multipal pesch word allow 
