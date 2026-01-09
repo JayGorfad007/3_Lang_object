cc25b32b42e1765c02e39fc8bcc9cf2c0db60643
js-code <jaygorfad5977@gmail.com>
2026-01-10 01:55:22 +0530

    changes

---
+++ C_Lang/4_4Q.c
@@ -0,0 +1,27 @@
+/*
+
+                                                    Author-:Jay Gorfad
+                                                    Doc-:15-12 Oct 2025
+                                                    Objective-:To Create a class
+                                                    Lacture-:4
+
+                                                    *
+                                                    **
+                                                    ***
+                                                    ****
+                                                    *****
+                                                    ******
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j;
+
+    for(i=1; i<=6; i++){
+        for(j=1; j<=i; j++){
+                printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
