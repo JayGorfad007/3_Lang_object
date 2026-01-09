507fc312105bc0ca07ea5ab7aa06f625de806760
js-code <jaygorfad5977@gmail.com>
2026-01-10 02:03:33 +0530

    right-angled triangle character pattern.

--- C_Lang/4_5Q.c
+++ C_Lang/4_5Q.c
@@ -5,21 +5,23 @@
                                                     Objective-:To Create a class
                                                     Lacture-:4
 
-                                                    *
-                                                    **
-                                                    ***
-                                                    ****
-                                                    *****
-                                                    ******
+                                                   Characgter -> ! @ # $ % & * print
 */
 #include<stdio.h>
 int main()
 {
     int i,j;
+    int row;
+    char ch;
+    printf("\nEnter the row");
+    scanf("%d",&row);
+    printf("\nEnter the Character");
+    scanf(" %c",&ch);
+    
 
-    for(i=1; i<=6; i++){
+    for(i=1; i<=row; i++){
         for(j=1; j<=i; j++){
-                printf("*");
+                printf("%c",ch);
         }
         printf("\n");
     } 
