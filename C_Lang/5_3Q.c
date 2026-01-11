650e717df09bf6c4e902f9cdc9d1862fe10e37ca
js-code <jaygorfad5977@gmail.com>
2026-01-12 05:11:41 +0530

    Horizontal and Vertical traingle

--- C_Lang/5_3Q.c
+++ C_Lang/5_3Q.c
@@ -9,9 +9,42 @@
 */
 
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
- 
-  
+    int i,j,k,l,t,c,r,choice;
+    start:
+    printf("\n1------Horizontal");
+    printf("\n2------Vertical traingle");
+    printf("\n3------Exit");
+    printf("\nEnter the choice");
+    scanf("%d",&choice);
+    switch(choice){
+        case 1:printf("");
+               for(l=1; l<=5; l++){
+                for(t=1; t<=5; t++){
+                 for(i=1; i<=l; i++){
+                    printf("*");
+                 }
+                 printf("\t");
+                }
+                printf("\n");
+               }// end of outer most
+             break; 
+        case 2:
+               for(t=1; t<=3; t++){
+                for(r=1; r<=5; r++){
+                 for(c=1; c<=r; c++){
+                    printf("*");
+                 }
+                 printf("\n");
+                }
+                printf("\n");
+               }// end of outer most
+             break;
+        case 3: exit(0);
+    }
+    return 0; 
+    goto start;
 }
 
