02f4be3e7f6559884251470aba35f66c5b079ecf
js-code <jaygorfad5977@gmail.com>
2026-01-21 16:32:53 +0530

    Program to Add Two 3×3 Matrices Using 2D Array in C

---
+++ C_Lang/9_1Mtrx.c
@@ -0,0 +1,76 @@
+/*
+                                                                 Author    : Jay Gorfad
+                                                                 Date      : 5-1-2026
+                                                                 Objective : To Matrix Two Addition Program   : Addition of Two 3×3 Matrices
+                                                                 Lecture   : 9
+                                                               
+*/
+#include<stdio.h>
+#define ROW 3
+#define COL 3
+#define SIZE 3
+int main()
+{
+    int a[ROW][COL];
+    int b[ROW][COL];
+    int c[ROW][COL];
+    int i,j,k;
+    
+    for(i=0; i<ROW; i++){
+        for(j=0; j<COL; j++){
+            printf("\nEnter the values of A[%d][%d] : ",i,j);
+            scanf("%d",&a[i][j]);
+        }//end of inner for
+
+    }//end of outer for 
+
+     for(i=0; i<ROW; i++){
+        for(j=0; j<COL; j++){
+            printf("\nEnter the values of B [%d][%d] : ",i,j);
+            scanf("%d",&b[i][j]);
+        }//end of inner for
+    }//end of outer for 
+
+     for(i=0; i<ROW; i++){
+        for(j=0; j<COL; j++){
+            printf("%d ",a[i][j]);
+    
+        }//end of inner for
+        printf("\n");
+    }//end of outer for 
+    printf("\n");
+
+    for(i=0; i<ROW; i++){
+        for(j=0; j<COL; j++){
+            printf("%d ",b[i][j]);
+    
+        }//end of inner for
+        printf("\n");
+    }//end of outer for
+    printf("\n");
+
+    for(i=0; i<ROW; i++){
+        for(j=0; j<COL; j++){
+            
+            c[i][j]=a[i][j]+b[i][j];
+    
+        }//end of inner for
+        printf("\n");
+    }//end of outer for
+  
+ 
+    for(i=0; i<ROW; i++){
+        for(j=0; j<COL; j++){
+
+            printf("%d ",c[i][j]);
+    
+        }//end of inner for
+
+        printf("\n");
+
+    }//end of outer for
+
+    
+
+    return 0;
+}
